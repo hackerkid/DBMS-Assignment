c54c7bf5051ec20397e40642b78586653d894d37
Vishnu Ks <yo@vishnuks.com>
2015-04-06 17:24:08 +0530

    bug

--- plsql/4.sql
+++ plsql/4.sql
@@ -4,8 +4,9 @@ ans number(8, 4);
 
 
 BEGIN
+
 	radius := 3;
-		WHILE ( 3 <= 7) LOOP
+		WHILE ( radius <= 7) LOOP
 			ans := 3.14 * radius;
 			INSERT INTO area VALUES(radius, ans);
 			radius := radius + 1;
